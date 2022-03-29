7c20eef0f6a7debe89c9db69f16508a1bfe2e388
George-Okumu <gokumu12@gmail.com>
2022-03-29 09:41:55 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+function submitForm(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                userName,
+                userEmail
+            }),
+        })
+        .then(response => response.json())
+        .then(user => console.log(user["userName"]))
+        .catch(error => console.log(error.message));
+}
+
+submitForm("George", "gokumu12@gmail.com");
